1f0eeab65378818db7fdc4d4eda7f43b100c5ed6
Reynaldo A. Baja Jr <reynaldobaja2@gmail.com>
2024-05-22 16:52:49 +0800

    Initial commit

--- api/app/Providers/RepositoryServiceProvider.php
+++ api/app/Providers/RepositoryServiceProvider.php
@@ -2,13 +2,19 @@
 
 namespace App\Providers;
 
+use App\Interface\Repository\OrderDetailRepositoryInterface;
+use App\Interface\Repository\OrderRepositoryInterface;
 use App\Interface\Repository\ProductRepositoryInterface;
 use App\Interface\Repository\UserRepositoryInterface;
 use App\Interface\Service\AuthenticationServiceInterface;
+use App\Interface\Service\OrderDetailServiceInterface;
+use App\Interface\Service\OrderServiceInterface;
 use App\Interface\Service\ProductServiceInterface;
+use App\Repository\OrderRepository;
 use App\Repository\ProductRepository;
 use App\Repository\UserRepository;
 use App\Service\AuthenticationService;
+use App\Service\OrderService;
 use App\Service\ProductService;
 use Illuminate\Support\ServiceProvider;
 
@@ -19,9 +25,13 @@ class RepositoryServiceProvider extends ServiceProvider
     {
 
         $this->app->bind(ProductRepositoryInterface::class, ProductRepository::class);
+        $this->app->bind(OrderRepositoryInterface::class, OrderRepository::class);
+        $this->app->bind(OrderDetailRepositoryInterface::class, OrderRepository::class);
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
 
         $this->app->bind(ProductServiceInterface::class, ProductService::class);
+        $this->app->bind(OrderServiceInterface::class, OrderService::class);
+        $this->app->bind(OrderDetailServiceInterface::class, OrderService::class);
         $this->app->bind(AuthenticationServiceInterface::class, AuthenticationService::class);
     }
 
--- api/routes/api.php
+++ api/routes/api.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\OrderDetailController;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\OrderDetailController;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\AuthController;
 
@@ -22,17 +22,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('orders', [OrderController::class, 'index']);
-Route::post('orders', [OrderController::class, 'create']);
-Route::get('orders/{id}', [OrderController::class, 'read']);
-Route::put('orders/{id}', [OrderController::class, 'update']);
-Route::delete('orders/{id}', [OrderController::class, 'delete']);
+Route::middleware('auth:sanctum')->prefix('orders')->group(function () {
+    Route::get('/', [OrderController::class, 'index']);
+    Route::get('/{id}', [OrderController::class, 'show']);
+    Route::post('/', [OrderController::class, 'store']);
+    Route::patch('/{id}', [OrderController::class, 'update']);
+    Route::delete('/{id}', [OrderController::class, 'destroy']);
+});
 
-Route::get('order_detail', [OrderDetailController::class, 'index']);
-Route::post('order_detail', [OrderDetailController::class, 'create']);
-Route::get('order_detail/{id}', [OrderDetailController::class, 'read']);
-Route::put('order_detail/{id}', [OrderDetailController::class, 'update']);
-Route::delete('order_detail/{id}', [OrderDetailController::class, 'delete']);
+Route::middleware('auth:sanctum')->prefix('OrderDetails')->group(function () {
+    Route::get('/', [OrderDetailController::class, 'index']);
+    Route::get('/{id}', [OrderDetailController::class, 'show']);
+    Route::post('/', [OrderDetailController::class, 'store']);
+    Route::patch('/{id}', [OrderDetailController::class, 'update']);
+    Route::delete('/{id}', [OrderDetailController::class, 'destroy']);
+});
 
 Route::middleware('auth:sanctum')->prefix('products')->group(function () {
     Route::get('/', [ProductController::class, 'index']);
@@ -41,7 +45,6 @@ Route::middleware('auth:sanctum')->prefix('products')->group(function () {
     Route::patch('/{id}', [ProductController::class, 'update']);
     Route::delete('/{id}', [ProductController::class, 'destroy']);
 });
-
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
