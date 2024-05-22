2b382aff39a362ee60cc06003f38b5003a51e2e8
Reynaldo A. Baja Jr <reynaldobaja2@gmail.com>
2024-05-22 17:10:21 +0800

    Rey create repository and service for Order and OrderDetails

--- api/app/Providers/RepositoryServiceProvider.php
+++ api/app/Providers/RepositoryServiceProvider.php
@@ -10,10 +10,12 @@ use App\Interface\Service\AuthenticationServiceInterface;
 use App\Interface\Service\OrderDetailServiceInterface;
 use App\Interface\Service\OrderServiceInterface;
 use App\Interface\Service\ProductServiceInterface;
+use App\Repository\OrderDetailRepository;
 use App\Repository\OrderRepository;
 use App\Repository\ProductRepository;
 use App\Repository\UserRepository;
 use App\Service\AuthenticationService;
+use App\Service\OrderDetailService;
 use App\Service\OrderService;
 use App\Service\ProductService;
 use Illuminate\Support\ServiceProvider;
@@ -26,12 +28,12 @@ class RepositoryServiceProvider extends ServiceProvider
 
         $this->app->bind(ProductRepositoryInterface::class, ProductRepository::class);
         $this->app->bind(OrderRepositoryInterface::class, OrderRepository::class);
-        $this->app->bind(OrderDetailRepositoryInterface::class, OrderRepository::class);
+        $this->app->bind(OrderDetailRepositoryInterface::class, OrderDetailRepository::class);
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
 
         $this->app->bind(ProductServiceInterface::class, ProductService::class);
         $this->app->bind(OrderServiceInterface::class, OrderService::class);
-        $this->app->bind(OrderDetailServiceInterface::class, OrderService::class);
+        $this->app->bind(OrderDetailServiceInterface::class, OrderDetailService::class);
         $this->app->bind(AuthenticationServiceInterface::class, AuthenticationService::class);
     }
 
